180a21f101dbc928825b8ed9d2acdaea71a53059
Bartlomiej Pietrowski <bartlomiej.pietrowski@capgemini.com>
2018-04-17 08:27:35 +0200

    An equivalent of famous two way data binding example.

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -4,12 +4,26 @@ import './Hello.css'
 
 export class Hello extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      who: props.who,
+    };
+  }
+
+  onWhoChange(e) {
+    e.preventDefault();
+    this.setState({
+        who: e.target.value,
+    });
+  }
+
   render() {
-    const { who } = this.props;
+    const { who } = this.state;
     return (
       <div className="hello-container">   
         <span className="hello">Hello {who}</span>
-        <input value={who} onChange={(e) => alert(e.target.value)} type="text"/>
+        <input value={who} onChange={(e) => this.onWhoChange(e)} type="text"/>
       </div>   
     );
   }
