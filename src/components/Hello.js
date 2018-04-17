a9784edb2e34bb8de7ade013ba7ba0b4a63762b6
Bartlomiej Pietrowski <bartlomiej.pietrowski@capgemini.com>
2018-04-17 07:54:43 +0200

    Introduce more styling - added grid to show later how it can be moved to App level if needed. Show how event handlers can be added, still no input value modification happens.

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 
+import './Hello.css'
+
 export class Hello extends Component {
+
   render() {
     const { who } = this.props;
-    return <span className="hello">Hello {who}</span>;
+    return (
+      <div className="hello-container">   
+        <span className="hello">Hello {who}</span>
+        <input value={who} onChange={(e) => alert(e.target.value)} type="text"/>
+      </div>   
+    );
   }
 }
