5cb383ee23f1e66e900cad1d79d75d6a44be48e1
Bartlomiej Pietrowski <bartlomiej.pietrowski@capgemini.com>
2018-04-17 06:57:53 +0200

    Switch to Hello World component, show JSX and pure JS implementation.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+
   render() {
     return (
-      <div className="book-app">
-        Hello World
-      </div>
-    );
+        <div className="book-app">
+          <span className="hello">
+            Hello World
+          </span>
+        </div>
+      );
+      
+    // return React.createElement('div', {className: 'book-app'},
+    //   React.createElement('span', {className: 'hello'}, 
+    //     'Hello World'
+    //   ),
+    // );
   }
 }
 
